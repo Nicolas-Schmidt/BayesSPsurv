3ae6236896e95f6415eb5cffccc4a13dbd63d5e6
Nicolas-Schmidt <nicoschlab@gmail.com>
2020-04-20 11:51:39 -0300

    SPsurv column 'S' missing

--- R/SPsurv.R
+++ R/SPsurv.R
@@ -22,23 +22,20 @@ SPsurv <- function(duration, immune, Y0, LY, data = list(), N, burn, thin, w = c
     data <- data
     equation1 <- as.character(duration)
     equation2<-as.character(immune)
-    formula1<-paste(equation1[2],equation1[1],equation1[3],sep="")
-    formula2<-paste(equation2[2],equation2[1],equation2[3],sep="")
-    mf1 <- model.frame(formula=as.formula(formula1), data=data)
-    mf2 <- model.frame(formula=as.formula(formula2), data=data)
-    X <- model.matrix(attr(mf1, "terms"), data=mf1)
-    Z <- model.matrix(attr(mf2, "terms"), data=mf2)
+    formula1 <- paste(equation1[2], equation1[1], equation1[3], sep = "")
+    formula2 <- paste(equation2[2], equation2[1], equation2[3], sep = "")
+    mf1 <- model.frame(formula = as.formula(formula1), data = data)
+    mf2 <- model.frame(formula = as.formula(formula2), data = data)
+    X <- model.matrix(attr(mf1, "terms"), data = mf1)
+    Z <- model.matrix(attr(mf2, "terms"), data = mf2)
     Y <- as.matrix(model.response(mf1))
     C <- as.matrix(model.response(mf2))
 
     Y0 <- as.character(Y0)
     LY <- as.character(LY)
-
-    dat <- subset(data, select=c(Y0, LY))
-
+    dat <- subset(data, select = c(Y0, LY))
     Y0 <- dat[,1]
     LY <- dat[,2]
-
     burn = burn
     if(is.null(w)){
         w = c(1,1,1)
@@ -51,11 +48,9 @@ SPsurv <- function(duration, immune, Y0, LY, data = list(), N, burn, thin, w = c
         m=m
     }
     form = form
-
-    dataset <- data.frame(cbind(Y, Y0, C, LY, S, X, Z))  # S ??
-    dataset  <- na.omit(dataset)
+    dataset <- data.frame(cbind(Y, Y0, C, LY, S, X, Z))  # S ?? <--
+    dataset <- na.omit(dataset)
     col <- ncol(dataset)
-
     Y  <- as.matrix(dataset[,1])
     Y0 <- as.matrix(dataset[,2])
     C  <- as.matrix(dataset[,3])
