ba7c9e06bf04d31f76aecc7f91e36a1ecb8d1f3c
Nicolas-Schmidt <nicoschlab@gmail.com>
2020-08-10 16:24:32 -0300

    doc Walter 2015 JCR

--- R/Walter_2015_JCR.R
+++ R/Walter_2015_JCR.R
@@ -26,4 +26,4 @@
 #' @usage data(Walter_2015_JCR)
 #' @format A data frame with 1562 rows and 13 variables
 #' @source Walter, Barbara F. (2015), Why Bad Governance Leads to Repeat Civil War,  Journal of Conflict Resolution 59(7), 1242 - 1272.
-
+NULL
