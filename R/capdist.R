ca245a2ba37942e68b812192e2265e4a948df827
Sergio Bejar <sbbejar@gmail.com>
2020-08-13 13:29:14 -0700

    Update capdist.R

--- R/capdist.R
+++ R/capdist.R
@@ -3,10 +3,10 @@
 #' Gleditsch and Ward Distance data
 #'
 #'\describe{
-#'    \item{numa}{...}
-#'    \item{ida}{...}
-#'    \item{numb}{...}
-#'    \item{idb}{...}
+#'    \item{numa}{COW code -- country A}
+#'    \item{ida}{Three letter ISO code -- country A}
+#'    \item{numb}{COW code -- country B}
+#'    \item{idb}{Three letter ISO code -- country B}
 #'    \item{kmdist}{...}
 #'    \item{midist}{...}
 #' }
