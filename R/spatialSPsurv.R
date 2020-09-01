eee84511f6fdd056dabab31a51d2043373a2e74e
Sergio Bejar <sbbejar@gmail.com>
2020-08-31 17:52:10 -0700

    ago31

--- R/spatialSPsurv.R
+++ R/spatialSPsurv.R
@@ -1,20 +1,20 @@
 #' @title spatialSPsurv
-#' @description Markov Chain Monte Carlo (MCMC) to run time-varying Bayesian split population survival model with spatial frailties
+#' @description Markov Chain Monte Carlo (MCMC) to run time-varying Bayesian split population survival model with spatial frailties.
 #'
-#' @param duration survival stage equation written in a formula of the form Y ~ X1 + X2 + ... where Y is duration until failure or censoring
-#' @param immune split stage equation written in a formula of the form C ~ Z1 + Z2 + ... where C is a binary indicator of immunity
-#' @param Y0 the elapsed time since inception until the beginning of time period (t-1)
-#' @param LY last observation year (coded as 1; 0 otherwise) due to censoring or failure
-#' @param data dataframe
-#' @param S spatial information (e.g. district ID) for each observation that matches the spatial matrix row/column information
-#' @param A an a times a spatial weights matrix where a is the number of unique spatial units (S) load as a separate file
-#' @param N number of MCMC iterations
-#' @param burn burn-in to be discarded
-#' @param thin thinning to prevent from autocorrelation
-#' @param w size of the slice in the slice sampling for (betas, gammas, rho). Write it as a vector. E.g. c(1,1,1)
-#' @param m limit on steps in the slice sampling. A vector of values for beta, gamma, rho
-#' @param form type of parametric model (Exponential, Weibull or Log-Logistic)
-#' @param prop.var proposal variance for Metropolis-Hastings
+#' @param duration survival stage equation written in a formula of the form Y ~ X1 + X2 + ... where Y is duration until failure or censoring.
+#' @param immune split stage equation written in a formula of the form C ~ Z1 + Z2 + ... where C is a binary indicator of immunity.
+#' @param Y0 the elapsed time since inception until the beginning of time period (t-1).
+#' @param LY last observation year (coded as 1; 0 otherwise) due to censoring or failure.
+#' @param data dataframe.
+#' @param S spatial information (e.g. district ID) for each observation that matches the spatial matrix row/column information.
+#' @param A an a times a spatial weights matrix where a is the number of unique spatial units (S) load as a separate file.
+#' @param N number of MCMC iterations.
+#' @param burn burn-in to be discarded.
+#' @param thin thinning to prevent from autocorrelation.
+#' @param w size of the slice in the slice sampling for (betas, gammas, rho). Write it as a vector. E.g. c(1,1,1).
+#' @param m limit on steps in the slice sampling. A vector of values for beta, gamma, rho.
+#' @param form type of parametric model (Exponential, Weibull or Log-Logistic).
+#' @param prop.var proposal variance for Metropolis-Hastings.
 #'
 #' @return chain of the variables of interest
 #'
