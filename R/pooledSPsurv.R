433a9ad27a0276a7e47fa7c132d56491b592d51a
Sergio Bejar <sbbejar@gmail.com>
2020-08-31 17:51:06 -0700

    ago31

--- R/pooledSPsurv.R
+++ R/pooledSPsurv.R
@@ -1,17 +1,17 @@
 #' @title pooledSPsurv
-#' @description Markov Chain Monte Carlo (MCMC) to run Bayesian split population survival model with no frailties
+#' @description Markov Chain Monte Carlo (MCMC) to run Bayesian split population survival model with no frailties.
 #'
-#' @param duration survival stage equation written in a formula of the form Y ~ X1 + X2 + ... where Y is duration until failure or censoring
-#' @param immune split stage equation written in a formula of the form C ~ Z1 + Z2 + ... where C is a binary indicator of immunity
-#' @param Y0 the elapsed time since inception until the beginning of time period (t-1)
-#' @param LY last observation year (coded as 1; 0 otherwise) due to censoring or failure
-#' @param data dataframe
-#' @param N number of MCMC iterations
-#' @param burn burn-in to be discarded
-#' @param thin thinning to prevent from autocorrelation
-#' @param w size of the slice in the slice sampling for (betas, gammas, rho). Write it as a vector. E.g. c(1,1,1)
+#' @param duration survival stage equation written in a formula of the form Y ~ X1 + X2 + ... where Y is duration until failure or censoring.
+#' @param immune split stage equation written in a formula of the form C ~ Z1 + Z2 + ... where C is a binary indicator of immunity.
+#' @param Y0 the elapsed time since inception until the beginning of time period (t-1).
+#' @param LY last observation year (coded as 1; 0 otherwise) due to censoring or failure.
+#' @param data dataframe.
+#' @param N number of MCMC iterations.
+#' @param burn burn-in to be discarded.
+#' @param thin thinning to prevent from autocorrelation.
+#' @param w size of the slice in the slice sampling for (betas, gammas, rho). Write it as a vector. E.g. c(1,1,1).
 #' @param m limit on steps in the slice sampling. A vector of values for beta, gamma, rho.
-#' @param form type of parametric model (Exponential, Weibull or Log-Logistic)
+#' @param form type of parametric model (Exponential, Weibull or Log-Logistic).
 #'
 #' @return chain of the variables of interest
 #'
